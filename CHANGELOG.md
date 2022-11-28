de8d27fe2e6fb5c36e21a78623f291c5a7b5e2dd
Julian Kimmig <julian.kimmig@at-bo.de>
2022-11-28 21:16:05 +0000

    fixes errors from marketplace and adds features: + Replace single project usage with issue id regex detection + Modified and add log information using vscode channel api + Fixes path errors when using windows + Adds remote loading of total ticket worklog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,33 +1,51 @@
 # Change Log
+
 All notable changes to the "vscode-jira-worklog" extension will be documented in this file.
 
+## 0.2.0
+
+- Replace single project usage with issue id regex detection
+- Modified and add log information using vscode channel api
+- Fixes path errors when using windows
+- Adds remote loading of total ticket worklog
+
 ## 0.1.14
+
 - Hide status bar item when you're not in a git repository
 
 ## 0.1.13
+
 - Show notification when you're not in a git repository
 
 ## 0.1.12
+
 - Some refactoring & auth after every init
 
 ## 0.1.11
+
 - Request is not defined bugfix
 
 ## 0.1.10
+
 - Some naming fixes
 
 ## 0.1.9
+
 - logwork -> worklog
 
 ## 0.1.8
+
 - Update extension name
 
 ## 0.1.7
+
 - Add extension icon
 
 ## 0.1.6
+
 - Change images urls
 - Add badge to readme.md
 
 ## 0.1.5
+
 - Refactoring
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-const vscode = require('vscode');
-const constants = require('./constants');
-const https = require('https');
+const vscode = require("vscode");
+const constants = require("./constants");
+const https = require("https");
 
 function getNormalizedTime(seconds) {
   function numberWithZero(number) {
@@ -17,62 +17,63 @@ const requestCreator = (login, context) => {
     const hostSetting = vscode.workspace.getConfiguration().get(constants.settings.jiraHostSettingKey);
     const basicAuthLoginSetting = vscode.workspace.getConfiguration().get(constants.settings.jiraProjectBasicAuthLoginKey);
     const basicAuthPasswordSetting = vscode.workspace.getConfiguration().get(constants.settings.jiraProjectBasicAuthPasswordKey);
-  
-    const jiraPort = hostSetting.indexOf('https://') > -1 ? 443: 80;
-    const hostSettingWithoutProtocol = hostSetting.replace(/https?:\/\//i, '');
+
+    const jiraPort = hostSetting.indexOf("https://") > -1 ? 443 : 80;
+    const hostSettingWithoutProtocol = hostSetting.replace(/https?:\/\//i, "");
     const postData = JSON.stringify(data);
     const session = context.globalState.get(constants.jiraSessionState);
-  
+
     return new Promise((resolve, reject) => {
-      const authRequest = https.request({
-        method,
-        port: jiraPort,
-        hostname: hostSettingWithoutProtocol,
-        path,
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(postData),
-          ...(session ? { 'Cookie': `JSESSIONID=${session}`} : {}),
-          ...(
-            basicAuthLoginSetting && basicAuthPasswordSetting ?
-              {
-                Authorization: `Basic ${Buffer.from(`${basicAuthLoginSetting}:${basicAuthPasswordSetting}`).toString('base64')}`
-              } :
-              {}
-            )
+      const authRequest = https.request(
+        {
+          method,
+          port: jiraPort,
+          hostname: hostSettingWithoutProtocol,
+          path,
+          headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(postData),
+            ...(session ? { Cookie: `JSESSIONID=${session}` } : {}),
+            ...(basicAuthLoginSetting && basicAuthPasswordSetting
+              ? {
+                  Authorization: `Basic ${Buffer.from(`${basicAuthLoginSetting}:${basicAuthPasswordSetting}`).toString("base64")}`,
+                }
+              : {}),
+          },
+        },
+        (response) => {
+          let dataQueue = "";
+          response.on("data", (chunk) => {
+            dataQueue += chunk;
+          });
+
+          response.on("end", async () => {
+            let jsonData;
+            if (dataQueue.indexOf("Unauthorized") > -1) {
+              await login(true);
+              jsonData = await request(path, method, data);
+              return resolve(jsonData);
+            }
+
+            jsonData = JSON.parse(dataQueue);
+            resolve(jsonData);
+          });
         }
-      }, (response) => {
-        let dataQueue = '';
-        response.on('data', (chunk) => {
-          dataQueue += chunk;
-        });
-  
-        response.on('end', async () => {
-          let jsonData;
-          if (dataQueue.indexOf('Unauthorized') > -1) {
-            await login(true);
-            jsonData = await request(path, method, data);
-            return resolve(jsonData);
-          }
-  
-          jsonData = JSON.parse(dataQueue);
-          resolve(jsonData);
-        });
-      });
-  
-      authRequest.on('error', (error) => {
+      );
+
+      authRequest.on("error", (error) => {
         reject(error);
       });
-  
+
       authRequest.write(postData);
       authRequest.end();
     });
   }
-  
+
   return request;
-}
+};
 
 module.exports = {
   getNormalizedTime,
-  requestCreator
-}
+  requestCreator,
+};
